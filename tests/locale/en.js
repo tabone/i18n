574d2778730d1f74be9b1a8ee850e7f13f29e91f
tabone <taboneluca3@gmail.com>
2015-05-10 13:36:45 +0200

    Create new tests and fix current ones.

--- tests/locale/en.js
+++ tests/locale/en.js
@@ -1,8 +1,14 @@
 module.exports = {
   'greetings': {
     'text': {
-      'hello': 'hi'
+      'hello': 'hi',
+      'welcome': 'Welcole %s'
     },
     'bye': 'bye'
+  },
+  'messages': {
+    'zero': 'No messages',
+    'one': '1 message',
+    'many': '%s messages'
   }
 }
--- tests/test.js
+++ tests/test.js
@@ -10,30 +10,50 @@ describe('i18n-light module', function() {
     beforeEach(function() {
       opts = {
         defaultLocale: 'en',
-        dir: path.join(__dirname, 'locale')
+        dir: path.join(__dirname, 'locale'),
+        fallback: false
       }
+    })
+
+    afterEach(function() {
+      i18n._config = {}
     })
 
     it('should throw an error if \'defaultLocale\' is not set', function() {
       delete opts.defaultLocale
       assert.throws(function() {
         i18n.configure(opts)
-      }, Error)
+      })
     })
 
     it('should throw an error if \'dir\' is not set', function() {
       delete opts.dir
       assert.throws(function() {
         i18n.configure(opts)
-      }, Error)
+      })
     })
 
-    it('should assign the value of the configuration inside the \'config\''
-        + ' object', function() {
+    it('should default the \'fallback\' value to true', function() {
+      delete opts.fallback
+      i18n.configure(opts)
+      assert(i18n._config.fallback === true)
+    })
+
+    it('should assign the config values to the one passed as parameter.'
+        , function() {
           i18n.configure(opts)
           assert((i18n._config.dir === opts.dir)
-              && (i18n._config.defaultLocale === opts.defaultLocale))
+              && (i18n._config.defaultLocale === opts.defaultLocale)
+              && (i18n._config.fallback === opts.fallback))
         })
+
+    it('should consider an empty string as not a valid config value '
+      + 'for required attributes.', function() {
+        opts.defaultLocale = ''
+        assert.throws(function() {
+          i18n.configure(opts)
+        })
+      })
   })
 
   describe('getContext method', function() {
@@ -43,12 +63,15 @@ describe('i18n-light module', function() {
         dir: path.join(__dirname, 'locale')
       }
       i18n.configure(opts)
+      i18n._context = {}
+      i18n._currentLocale = opts.defaultLocale
     })
 
-    it('should include the json from the currentLocale json file inside '
-        + 'the \'_context\' instance property', function() {
-      i18n._currentLocale = opts.defaultLocale
+
+    it('should store the dictionary of the current locale inside the '
+        + '\'_context\' instance attribute.', function() {
       i18n._getContext()
+      assert(i18n._context[opts.defaultLocale] !== undefined)
     })
   })
 
@@ -61,7 +84,7 @@ describe('i18n-light module', function() {
       i18n.configure(opts)
     })
 
-    it('should return the current locale', function() {
+    it('should return the current locale.', function() {
       assert(i18n.getLocale() === opts.defaultLocale)
     })
   })
@@ -73,16 +96,74 @@ describe('i18n-light module', function() {
         dir: path.join(__dirname, 'locale')
       }
       i18n.configure(opts)
+      i18n._context['it']
     })
 
-    it('should change the current locale to the one specified', function() {
+    it('should change the current locale to the one specified.', function() {
       i18n.setLocale('it')
       assert(i18n._currentLocale === 'it')
     })
 
-    it('should get the context of the newly specified locale', function() {
+    it('should get the context of the newly specified locale.', function() {
       i18n.setLocale('it')
       assert(i18n._context['it'] !== undefined)
+    })
+  })
+
+  describe('_translate method', function() {
+    beforeEach(function() {
+      opts = {
+        defaultLocale: 'en',
+        dir: path.join(__dirname, 'locale')
+      }
+      i18n.configure(opts)
+
+      i18n._currentLocale = opts.defaultLocale
+      i18n._getContext()
+    })
+
+    describe('when current locale = default locale', function() {
+      it('should return the value if the path is valid', function() {
+        assert(i18n._translate('greetings.text.hello')
+            === i18n._context.en.greetings.text.hello)
+      })
+
+      it('should return the path specified if the path is invalid', function() {
+        assert(i18n._translate('greetings.hello')
+            === 'greetings.hello')
+      })
+    })
+
+    describe('when current locale <> default locale', function() {
+      it('should return the value if the path is valid.', function() {
+        i18n.setLocale('it')
+        assert(i18n._translate('greetings.text.hello') === 'ciao')
+      })
+
+      describe('if path is invalid'
+          , function() {
+            describe('when \'fallback\' config is true.', function() {
+              beforeEach(function() {
+                i18n._config.fallback = true
+              })
+              it('should try to display the value of the default locale.', 
+                  function() {
+                    i18n.setLocale('it')
+                    assert(i18n._translate('greetings.bye') === 'bye')
+                  })
+            }) 
+
+            describe('when \'fallback\' config is false.', function() {
+              beforeEach(function() {
+                i18n._config.fallback = false
+              })
+              it('it should display the path specified.', 
+                  function() {
+                    i18n.setLocale('it')
+                    assert(i18n._translate('greetings.bye') === 'greetings.bye')
+                  })
+            })
+          })
     })
   })
 
@@ -98,49 +179,67 @@ describe('i18n-light module', function() {
       i18n._getContext()
     })
 
-    describe('when used with the default locale', function() {
-      it('should return the value if the path is valid', function() {
+    describe('when path is only provided', function() {
+      it('should return the translated phrase', function() {
         assert(i18n.__('greetings.text.hello')
             === i18n._context.en.greetings.text.hello)
       })
+    })
 
-      it('should return the path specified if the path is invalid', function() {
-        assert(i18n.__('greetings.hello')
-            === 'greetings.hello')
+    describe('when sprintf placeholders are also provided', function() {
+      it('should return', function () {
+        assert(i18n.__('greetings.text.welcome', 'luca')
+            === i18n._context.en.greetings.text.welcome
+                .replace('%s', 'luca'))
+      })
+    })
+  })
+
+  describe('__n method', function() {
+    beforeEach(function() {
+      opts = {
+        defaultLocale: 'en',
+        dir: path.join(__dirname, 'locale')
+      }
+      i18n.configure(opts)
+
+      i18n._currentLocale = opts.defaultLocale
+      i18n._getContext()
+    })
+
+    describe('when path is only provided', function() {
+      it('should return the translated phrase', function() {
+        assert(i18n.__n('greetings.text.hello')
+            === i18n._context.en.greetings.text.hello)
       })
     })
 
-    describe('when used with a non default locale', function() {
-      it('if locale is changed, it should return the proper value for '
-          + 'the locale just configured', function() {
-            i18n.setLocale('it')
-            assert(i18n.__('greetings.text.hello') === 'ciao')
-          })
+    describe('when last parameter is not a number', function() {
+      it('should throw an error', function() {
+        assert.throws(function() {
+          i18n.__n('greetings.text.hello', 'text')
+        })
+      })
+    })
 
-      describe('if the path doesn\'t exists inside the context of the non '
-          + 'default locale', function() {
-            describe('when \'fallback\' config is true', function() {
-              beforeEach(function() {
-                i18n._config.fallback = true
-              })
-              it('should try to display the value of the default one', 
-                  function() {
-                    i18n.setLocale('it')
-                    assert(i18n.__('greetings.bye') === 'bye')
-                  })
-            }) 
+    describe('when last parameter is a number', function() {
+      describe('and it is == 0', function() {
+        it('should return the ', function() {
+          assert(i18n.__n('messages', 0) === 'No messages')
+        })
+      })
 
-            describe('when \'fallback\' config is false', function() {
-              beforeEach(function() {
-                i18n._config.fallback = false
-              })
-              it('it should display the path specified', 
-                  function() {
-                    i18n.setLocale('it')
-                    assert(i18n.__('greetings.bye') === 'greetings.bye')
-                  })
-            })
-          })
+      describe('and it is == 1', function() {
+        it('should return the ', function() {
+          assert(i18n.__n('messages', 1) === '1 message')
+        })
+      })
+
+      describe('and it is > 1', function() {
+        it('should return the ', function() {
+          assert(i18n.__n('messages', 2, 2) === '2 messages')
+        })
+      })
     })
   })
 })
